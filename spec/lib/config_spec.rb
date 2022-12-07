2bfe457448ba1cf9a45b11152ccee4c5808031e6
tfuda <tom@patrontechnology.com>
2022-12-07 10:32:38 -0500

    PMGR-10355 Change default API version to 56.0 and fix tests accordingly.

--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Metaforce do
   describe '.configuration' do
     subject { Metaforce.configuration }
-    it { should set_default(:api_version).to('30.0') }
+    it { should set_default(:api_version).to('56.0') }
     it { should set_default(:host).to('login.salesforce.com') }
-    it { should set_default(:endpoint).to('https://login.salesforce.com/services/Soap/u/30.0') }
-    it { should set_default(:partner_wsdl).to(File.expand_path('../../../wsdl/30.0/partner.xml', __FILE__)) }
-    it { should set_default(:metadata_wsdl).to(File.expand_path('../../../wsdl/30.0/metadata.xml', __FILE__)) }
+    it { should set_default(:endpoint).to('https://login.salesforce.com/services/Soap/u/56.0') }
+    it { should set_default(:partner_wsdl).to(File.expand_path('../../../wsdl/56.0/partner.xml', __FILE__)) }
+    it { should set_default(:metadata_wsdl).to(File.expand_path('../../../wsdl/56.0/metadata.xml', __FILE__)) }
   end
 end
